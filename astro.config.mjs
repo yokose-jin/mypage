e677399e596a36f21c145bfa2a69be8238e39598
横瀬仁 <jin@yokosehitoshinoMacBook-Air.local>
2025-10-13 13:36:39 +0900

    create github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    site: 'https://yokose-jin.github.io',
+    base: '/mypage',
 });
