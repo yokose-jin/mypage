d9e62077ae0aaf10343d985d1fb3f99f05ad5699
横瀬仁 <jin@yokosehitoshinoMacBook-Air.local>
2025-10-15 22:09:47 +0900

    add latex envs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import mdx from '@astrojs/mdx';
 // import shiki from '@astrojs/markdown-remark';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yokose-jin.github.io',
     base: '/mypage',
+    integrations: [mdx()],
     markdown: {
         remarkPlugins: [remarkMath],
         rehypePlugins: [rehypeKatex],
